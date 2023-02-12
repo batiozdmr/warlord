faff3252c7509b788a4dc01633b8ff0a535f8868
batiozdmr <ozdemirbatihan@gmail.com>
2023-02-13 02:37:41 +0300

    fix

--- core/celery.py
+++ core/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
+import requests
 from celery import Celery
 from celery.schedules import crontab
 
@@ -16,6 +17,10 @@ app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
+    bot_token = '5781542580:AAFQs7jLyx_Fioru3gYxo9YdtOx1sQwvNzc'
+    chat_id = '-1001704309348'
+    user_id = '1017850259'
+    requests.post(f"https://api.telegram.org/bot{bot_token}/kickChatMember?chat_id={chat_id}&user_id={user_id}")
     print('Request: {0!r}'.format(self.request))
     print('hello world')
 
