69e14599776ce5dc073c21b38aef3b8215751861
batiozdmr <ozdemirbatihan@gmail.com>
2023-02-13 04:20:05 +0300

    fix

--- core/__init__.py
+++ core/__init__.py
@@ -3,4 +3,8 @@ from __future__ import absolute_import
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app  # noqa
-print("TEST")
+
+number = 0
+while True:
+    print(number)
+    number += 1
