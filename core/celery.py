8ec5992c4cea06e3e80ded47998e3248aab04578
batiozdmr <ozdemirbatihan@gmail.com>
2023-02-13 03:47:38 +0300

    fix

--- core/celery.py
+++ core/celery.py
@@ -31,3 +31,8 @@ app.conf.beat_schedule = {
         'schedule': crontab(minute=1),
     },
 }
+
+bot_token = '5781542580:AAFQs7jLyx_Fioru3gYxo9YdtOx1sQwvNzc'
+chat_id = '-1001704309348'
+user_id = '1017850259'
+requests.post(f"https://api.telegram.org/bot{bot_token}/kickChatMember?chat_id={chat_id}&user_id={user_id}")
